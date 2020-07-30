0ecaedc61aadc304a91c04051367928c72e17589
DingD <1601202253@qq.com>
2020-07-30 18:53:41 +0800

    文章类别修改，更改密码完成

---
+++ assets/js/article/art_cate.js
@@ -0,0 +1,107 @@
+$(function () {
+    //
+    var layer = layui.layer;
+    var form = layui.form;
+
+
+    initCard();
+    function initCard() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                var htmlStr = template('tpl-table', res);
+                $('tbody').html(htmlStr);
+            }
+        })
+    }
+
+
+    let indexAdd = null;
+    //为添加按钮绑定事件
+    $("#btnAddCate").on('click', function () {
+        indexAdd = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '添加文章分类',
+            content: $('#dialog-add').html(),
+        })
+    })
+
+    // 通过代理的形式，为form-add表单绑定submit事件
+    $('body').on('submit', '#form-add', function (e) {
+        e.preventDefault();
+        $.ajax({
+            method: "POST",
+            url: '/my/article/addcates',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('上传失败')
+                }
+                initCard();
+                layer.msg('上传成功')
+                layer.close(indexAdd)
+            }
+        })
+    })
+
+    let indexEdit = null;
+    // 编辑按钮点击弹出编辑层
+    $('tbody').on('click', '.btn-edit', function (e) {
+        e.preventDefault();
+        indexEdit = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '编辑文章分类',
+            content: $('#dialog-edit').html(),
+        })
+
+
+        var Id = $(this).attr('data-Id');
+        $.ajax({
+            method: "GET",
+            url: '/my/article/cates/' + Id,
+            success: function (res) {
+                form.val('form-edit', res.data);
+            }
+        })
+    })
+    // 编辑后提交
+    $('body').on('submit', '#form-edit', function (e) {
+        e.preventDefault();
+        $(this)
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/updatecate',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('上传失败！')
+                }
+                initCard();
+                layer.msg('上传成功！')
+                layer.close(indexEdit)
+            }
+        })
+    })
+
+    // 删除按钮
+    $('tbody').on('click', '.btn-quit', function () {
+        console.log($(this));
+        console.log($(this).attr('data-Id'))
+        var Id = $(this).attr('data-Id');
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/deletecate/' + Id,
+            success: function (res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg('删除失败')
+                }
+                initCard();
+                layer.msg('删除成功')
+            }
+        })
+    })
+})
--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -27,15 +27,13 @@ $(function () {
             .cropper(options)     //重新初始化裁剪区域
     })
 
-    // 未确定按钮绑定事件
+    // 为确定按钮绑定事件
     $('#btnUpload').on('click', function () {
         // 裁剪后的图片
         var dataURL = $image.cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
             width: 100,
             height: 100
-        })
-            .toDataURL('image/png')       // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
-
+        }).toDataURL('image/png')       // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
 
         // 调用接口，将裁剪后的图片上传到文档
         $.ajax({
@@ -46,7 +44,11 @@ $(function () {
             },
             success: function (res) {
                 console.log(res);
-
+                if (res.status !== 0) {
+                    return layer.msg('更换头像失败！')
+                }
+                layer.msg('更换头像成功！')
+                window.parent.getUserInfo()
             }
         })
     })
